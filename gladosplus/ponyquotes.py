17497c16d9e65b1b5be8c12186d05c905d6bca80
Avalander <darth.quesorico@gmail.com>
2017-09-23 10:09:09 +0200

    Add pony quotes api

---
+++ gladosplus/ponyquotes.py
@@ -0,0 +1,29 @@
+import json
+import urllib
+
+import glados
+
+
+PONY_QUOTES_API = 'http://api.ponyquotes.com/v1'
+
+class PonyQuotes(glados.Module):
+    def get_help_list(self):
+        return [
+            glados.Help('pquote', '<pony?>', 'Returns a pony quote.')
+        ]
+
+    @glados.Module.commands('pquote')
+    def pony_quote(self, message, content):
+        query_string = urllib.parse.urlencode({
+            'pony': content
+        }) if content else ''
+        result = get_json_response(PONY_QUOTES_API + query_string)
+        if result['error']:
+            yield from self.client.send_message(message.channel, 'No quotes found.')
+        else:
+            yield from self.client.send_message(message.channel,
+                '"{quote}" --*{pony}*'.format(**result))
+
+def get_json_response(url):
+    with urllib.request.urlopen(url) as response:
+        return json.loads(response)
